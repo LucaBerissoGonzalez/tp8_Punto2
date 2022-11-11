203504b17a28d2544b6799e9a379e2dbd329f47b
lucaberissogonzalez <lucaberisso1@gmail.com>
2022-11-11 11:16:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tp8_Punto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int menosUno = -1;
+            const int dias = 5;
+
+            string[] diasSemana = {"Lunes", "Martes", "Miercoles", "jueves", "Viernes"};
+
+            for (int i = dias-1; i > menosUno; i--)
+            {
+                Console.WriteLine("Elemento " + i + ": " + diasSemana[i]);
+
+            }
+
+            Console.WriteLine("Presione una tecla para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
